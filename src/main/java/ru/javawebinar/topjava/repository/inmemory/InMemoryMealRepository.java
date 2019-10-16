89e4af1468e0295dd1b33593451a4cfa44d4c5d4
Dmitry Shaposhnikov <dims1001@yandex.ru>
2019-10-16 12:18:29 +0300

    use one map instead of two for InMemoryMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -9,62 +9,84 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
+
+    private class MealWithUid {
+        private Meal meal;
+        private int uid;
+
+        Meal getMeal() {
+            return meal;
+        }
+
+        void setMeal(Meal meal) {
+            this.meal = meal;
+        }
+
+        int getUid() {
+            return uid;
+        }
+
+        MealWithUid(Meal meal, int uid) {
+            this.meal = meal;
+            this.uid = uid;
+        }
+    }
+
     private AtomicInteger counter = new AtomicInteger(0);
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
-    private Map<Integer, Set<Integer>> uidToIds = new ConcurrentHashMap<>();
+    private Map<Integer, MealWithUid> repository = new ConcurrentHashMap<>();
 
     @Override
     public Meal save(Meal meal, int userId) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-
-            Set<Integer> ids = uidToIds.computeIfAbsent(userId, uid -> new HashSet<>());
-            ids.add(meal.getId());
-
+            repository.put(meal.getId(), new MealWithUid(meal, userId));
             return meal;
         }
 
-        if (!getUserMealsIds(userId).contains(meal.getId())) {
+        MealWithUid mealWithUid = repository.get(meal.getId());
+        if (mealWithUid.getUid() != userId) {
             return null;
         }
+        mealWithUid.setMeal(meal);
 
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        if (!getUserMealsIds(userId).contains(id)) {
+
+        MealWithUid mealWithUid = repository.get(id);
+        if (mealWithUid == null || mealWithUid.getUid() != userId) {
             return false;
         }
 
-        repository.remove(id);
-        uidToIds.get(userId).remove(id);
+        if (repository.remove(id) == null) {
+            throw new ConcurrentModificationException("Meal with id " + id + " was deleted in parallel request");
+        }
 
         return true;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        if (!getUserMealsIds(userId).contains(id)) {
+        MealWithUid mealWithUid = repository.get(id);
+        if (mealWithUid == null) {
             return null;
         }
-        return repository.get(id);
+        return mealWithUid.getUid() == userId
+                ? mealWithUid.getMeal()
+                : null;
     }
 
     @Override
     public Collection<Meal> getAll(int userId) {
-        Set<Integer> userMealsIds = getUserMealsIds(userId);
         return repository
                 .values()
                 .stream()
-                .filter(meal -> userMealsIds.contains(meal.getId()))
+                .filter(mealWithUid -> mealWithUid.getUid() == userId)
+                .map(MealWithUid::getMeal)
                 .sorted(Comparator.comparing(Meal::getDate).reversed().thenComparing(Meal::getId))
                 .collect(Collectors.toList());
-    }
-
-    private Set<Integer> getUserMealsIds(int userId) {
-        return uidToIds.getOrDefault(userId, new HashSet<>());
     }
 }
 

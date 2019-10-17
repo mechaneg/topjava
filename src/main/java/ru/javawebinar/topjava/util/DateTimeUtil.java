c38ddc7afc1cd9a10f7e549b03a107a35efcb9f6
Dmitry Shaposhnikov <dims1001@yandex.ru>
2019-10-17 18:00:28 +0300

    use DateTimeUtil generic method isBetween everywhere in project

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,22 +1,12 @@
 package ru.javawebinar.topjava.util;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return isBetweenGeneric(lt, startTime, endTime);
-    }
-
-    public static boolean isBetween(LocalDate ld, LocalDate startDate, LocalDate endDate) {
-        return isBetweenGeneric(ld, startDate, endDate);
-    }
-
-    private static <T extends Comparable<? super T>> boolean isBetweenGeneric(T val, T from, T to) {
+    public static <T extends Comparable<? super T>> boolean isBetween(T val, T from, T to) {
         return val.compareTo(from) >= 0 && val.compareTo(to) <= 0;
     }
 
